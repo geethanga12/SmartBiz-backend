02b40a1c5f5e09821501f5db9dbe3ec1c388723b
Geethanga <dissanayakegeethanga@gmail.com>
2025-08-04 01:25:10 +0530

    create SmartBiz backend

---
+++ src/main/java/lk/acpt/smartbiz/SmartbizApplication.java
@@ -0,0 +1,13 @@
+package lk.acpt.smartbiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/acpt/smartbiz/SmartbizApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.acpt.smartbiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
